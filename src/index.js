071f0e7a99e3208a33bc0011cbe4b1eb9d48ee7a
土岐将也 <dokimasaya@tokimasayanoMacBook-Air.local>
2023-07-05 04:25:05 +0900

    section4_20

--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+export const INCREMENT = 'INCREMENT'
+export const DECREMENT = 'DECREMENT'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+export const increment = () => ({
+    type: 'INCREMENT'
+})
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+export const decrement = () => ({
+    type: 'DECREMENT'
+})
