7529f4c411041a94e79c86eef97a5812236a40eb
土岐将也 <dokimasaya@tokimasayanoMacBook-Air.local>
2023-07-05 09:50:58 +0900

    section4_22

--- src/index.js
+++ src/index.js
@@ -8,3 +8,30 @@ export const increment = () => ({
 export const decrement = () => ({
     type: 'DECREMENT'
 })
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore } from 'react-redux'
+import { Provider } from 'react-redux'
+
+import './index.css';
+import reducer from './reducers'
+import App from './components/App';
+import registerServiceWorker from './registerServiceWorker';
+
+const store = createStore(reducer)
+
+ReactDOM.render(
+<Provider store={store}>
+ <App />
+</Provider>, 
+document.getElementById('root')
+);
+registerServiceWorker();
+
+
+
+
+
+
+
